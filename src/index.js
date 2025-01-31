baad2f30bbd1b0729b2cc8f7b5d926518a611e91
josdic1 <joshuadicker@gmail.com>
2025-01-31 17:27:07 -0500

    created starter template for project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const init = () => {
+  // DOM Elements
+
+
+  // Stateful Variables
+
+
+  // Fetch list on load
+
+
+  // Event Listeners
+
+
+  /** --------------------- HANDLER FUNCTIONS --------------------- **/
+
+
+  /** --------------------- RENDER FUNCTIONS --------------------- **/
+
+
+  /** --------------------- API FUNCTIONS --------------------- **/
+
+
+
+}
+
+window.addEventListener("DOMContentLoaded", init)
+
+
